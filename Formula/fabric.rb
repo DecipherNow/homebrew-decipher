fc69bfe9edb4324f9c39489c9d9b6ec2d23dae51
Joshua Mark Rutherford <joshua.rutherford@deciphernow.com>
2018-01-11 21:27:30 -0500

    Issue #4: Bump fabric to v0.1.0

--- Formula/fabric.rb
+++ Formula/fabric.rb
@@ -2,7 +2,10 @@ class Fabric < Formula
   desc "Command-line utilities for Grey Matter Fabric"
   homepage "https://github.com/deciphernow/gm-fabric-go.git"
 
-  head "https://github.com/deciphernow/gm-fabric-go", :using => :git
+  url "https://github.com/DecipherNow/gm-fabric-go/archive/v0.1.0.tar.gz"
+  sha256 "76a3009c2dc9d748aaed19eba29d5bb85e579bdadb1d779860d0475312ae4ddb"
+
+  head "https://github.com/deciphernow/gm-fabric-go.git"
 
   depends_on "dep" => :build
   depends_on "go" => [:build, :recommended]
@@ -16,7 +19,7 @@ class Fabric < Formula
     ENV["GOPATH"] = gopath
 
     cd gopath/"src/github.com/deciphernow/gm-fabric-go/cmd/fabric" do
-      hash = `git rev-parse --verify --short HEAD`
+      hash = build.head? ? `git rev-parse --verify --short HEAD` : 'release'
       system "dep", "ensure", "-v"
       system "go", "build", "-o", "fabric", "-ldflags", "-X github.com/deciphernow/gm-fabric-go/version.gitHash=#{hash}"
       bin.install "fabric"
